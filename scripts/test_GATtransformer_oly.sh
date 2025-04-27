214d3e1dffcb2c461d12b5e3e89d5c668f6a3003
Sandipan Sarma <sandipan.sarma@iitg.ac.in>
2025-04-27 16:02:43 +0530

    Delete scripts/test_GATtransformer_oly.sh

--- scripts/test_GATtransformer_oly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /workspace/udit/arijit/sandipan/zsar_divyam/own_zsar/kg_gat
-
-# python3 main_kg.py --mode=gat_lca_test --dataset=olympics --network=clip_transformer_classifier --semantic=clip --image_size=224 --save_path='../../log/log_transformer_olympics9/' --split_index=9
-
-python3 main_kg.py --mode=gat_lca_test --dataset=olympics --network=clip_transformer_classifier --semantic=clip --image_size=224 --save_path='../../log/log_transformer_olympics2_retry1/' --split_index=2
-
-python3 main_kg.py --mode=gat_lca_test --dataset=olympics --network=clip_transformer_classifier --semantic=clip --image_size=224 --save_path='../../log/log_transformer_olympics3_retry1/' --split_index=3
